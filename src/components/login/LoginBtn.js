2f24fc2eb30745f949dc7a742de2e2f91d8bb7d1
Cocomong98 <yonghjjm0618@gmail.com>
2024-05-22 01:07:14 +0900

    240522

--- src/components/login/LoginBtn.js
+++ src/components/login/LoginBtn.js
@@ -7,11 +7,12 @@ function LoginBtn() {
 
     const handleSignInClick = () => {
         // 서버 배포용
-        // window.location.href =
-        //     "https://walab.info:8443/HisnetLogin/hisnet-login?accessKey=LByzLqghXnUp0SHHbHiI&returnUrl=https://walab.info/HappyMan/login-ing";
-        // 로컬 개발용
+        console.log("로그인 버튼 클릭");
         window.location.href =
-            "https://walab.info:8443/HisnetLogin/hisnet-login?accessKey=LByzLqghXnUp0SHHbHiI&returnUrl=http://localhost:3000/HappyMan/login-ing";
+            "https://walab.info:8443/HisnetLogin/hisnet-login?accessKey=LByzLqghXnUp0SHHbHiI&returnUrl=https://walab.info/HappyMan/login-ing";
+        // 로컬 개발용
+        // window.location.href =
+        //     "https://walab.info:8443/HisnetLogin/hisnet-login?accessKey=LByzLqghXnUp0SHHbHiI&returnUrl=http://localhost:3000/HappyMan/login-ing";
 
         // navigate("/swap/sign-in"); // '/swap/sign-in'으로 페이지 이동을 합니다.
     };
